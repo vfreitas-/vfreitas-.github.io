7a937a4685834e366f4c32314f95406564d0929f
Vitor Freitas Buchalla <vitor_freitas_b@hotmail.com>
2020-09-11 16:51:39 -0300

    feat: add gh-pages-clean script and make it possible to access draft posts through url

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,7 +9,6 @@ exports.createPages = async ({ graphql, actions }) => {
     `
       {
         allMarkdownRemark(
-          filter: { fields: { draft: { eq: false } } }
           sort: { fields: [frontmatter___date], order: DESC }
           limit: 1000
         ) {
